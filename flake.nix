32ae7f4ab9be138adaf1c46e42f1916706feb274
Po Co <polniolino@gmail.com>
2026-09-08 22:41:36 +0200

    Initial commit
    bla

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              cargo
+              rustc
+              clippy
+            ];
+          };
+        }
+      );
+    };
+}
